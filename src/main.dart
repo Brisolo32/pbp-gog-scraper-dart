8204147726f06bba47f1a6bf6dd7a1d50abd8aa8
Davi Santos <brisolo32@outlook.com>
2023-01-04 13:46:29 -0300

    Changed file name

--- src/main.dart
+++ src/main.dart
@@ -3,35 +3,33 @@ import 'dart:io';
 import "package:http/http.dart" as http;
 
 String returnString() {
-  print('Cache location wasn\'t provided, using: "./cache.json"');
-  return "./cache.json";
+  print('Cache location wasn\'t provided, using: "./"');
+  return "./";
 }
 
 void main(List<String> args) async {
   // Checks if no arguments were provided
   if (args.isEmpty) {
-    print('Usage: "Game query (ex: terraria)" "Cache location (defaults to: "./cache.json")"\nProgram didn\'t run because no arguments were provided');
+    print(
+        'Usage: "Game query (ex: terraria)" "Cache location (defaults to: "./")"\nProgram didn\'t run because no arguments were provided');
     exit(1);
   }
 
   // Arguments
   String query = args[0];
-  String cache = !args.asMap().containsKey(1) ?  returnString() : args[1];
+  String cache = !args.asMap().containsKey(1) ? returnString() : args[1];
 
   Map uriData = {
     'uri': 'embed.gog.com',
     'endpoint': '/games/ajax/filtered',
-    'params': {
-      'mediaType': 'game',
-      'search': query
-    }
+    'params': {'mediaType': 'game', 'search': query}
   };
 
   // Fetches data
   Uri url = Uri.https(uriData['uri'], uriData['endpoint'], uriData['params']);
   var res = await http.get(url);
   Map json = jsonDecode(res.body);
-  var products = json['products'];
+  dynamic products = json['products'];
 
   // If 'products' is empty print an error message
   // And exit with error code 1
@@ -40,30 +38,21 @@ void main(List<String> args) async {
     exit(1);
   }
 
-  Map<String, dynamic> resData = {
-    'response': [
-    ]
-  };
+  Map<String, dynamic> resData = {'response': []};
 
   // Runs over a loop of all 'products' entries
   // Then adds the data to a Map and adds it to resData
-  for(int i = 0; i < products.length; i++) {
+  for (int i = 0; i < products.length; i++) {
     Map prod = json['products'][i];
     Map data = {
-      'gameDev': prod['developer'],
-      'gamePub': prod['publisher'],
-      'gameTitle': prod['title'],
-      'price': '${prod['price']['symbol']}${prod['price']['amount']}',
-      'basePrice': '${prod['price']['symbol']}${prod['price']['baseAmount']}',
-      'URL': 'https://gog.com${prod['url']}'
+      'title': prod['title'],
+      'urls': ["https://gog.com${prod["url"]}"]
     };
 
     resData['response'].add(data);
   }
 
   // Writes the file to the cache and then logs a message
-  dynamic file = File(cache);
+  dynamic file = File("$cache/results.json");
   file.writeAsString(jsonEncode(resData));
-
-  print('Wrote data to "$cache"');
 }
